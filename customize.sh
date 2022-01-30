9887fbc082fd04faeca03177e7cb5e1c0eebe659
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-01-30 17:08:29 +0800

    customize.sh: Check for outdated Magisk version

--- customize.sh
+++ customize.sh
@@ -7,5 +7,14 @@
 #     for all directories in <directory> (including itself), it will call:
 #        set_perm dir owner group dirpermission context
 
+outdated_magisk()
+{
+	ui_print "*******************************"
+	ui_print " Please install Magisk v24.0+! "
+	ui_print "*******************************"
+	abort
+}
+[ $MAGISK_VER_CODE -lt 24000 ] && outdated_magisk
+
 set_perm_recursive $MODPATH/system/etc 0 0 0755 0644
 set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
